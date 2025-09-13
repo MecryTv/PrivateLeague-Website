51bee49f9b1111a4f4ea3927f8e2cbc30d21bb4e
MecryTv <mail.MecryTv@gmail.com>
2025-09-14 00:38:25 +0100

    Add API

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+        port: 3000,
+        proxy: {
+            // Proxy für lokale Backend-API (entwickeln)
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false
+            }
+        }
+    }
 })
