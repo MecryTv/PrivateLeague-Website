e4727329831cdb466180bde5c036cae0141dcc3f
MecryTv <mail.MecryTv@gmail.com>
2025-09-14 00:32:49 +0100

    Add Basic

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,21 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "2.2.0"
+    kotlin("plugin.spring") version "2.2.0"
+    id("org.springframework.boot") version "3.5.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "de.mecrytv"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    // Spring Boot starters
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // Hinzugefügt für JPA
+
+    // Kotlin extras
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // Database
+    runtimeOnly("com.mysql:mysql-connector-j") // Hinzugefügt für MySQL
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(kotlin("test"))
+}
+
+kotlin {
+    // Toolchain für Java/Kotlin-Compiler
+    jvmToolchain(21)
+
+    // Neue compilerOptions-DSL (statt veraltetem kotlinOptions)
+    compilerOptions {
+        // jvmTarget ist durch jvmToolchain abgedeckt; hier setzen wir nur die freien Compiler-Args
+        freeCompilerArgs.add("-Xjsr305=strict")
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
---
+++ backend/src/main/kotlin/de/mecrytv/Application.kt
@@ -0,0 +1,11 @@
+package de.mecrytv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrivateLeagueApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrivateLeagueApplication>(*args)
+}
